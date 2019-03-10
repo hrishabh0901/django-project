e8e7e8b0ee8022aad29b1d7fdf62da051cbedfb5
hrishabh <hrishabh@gmail.com>
2019-03-11 01:17:09 +0530

    Done

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+
+    def pretifiy(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
+    def fullblog(self):
+        return self.body
